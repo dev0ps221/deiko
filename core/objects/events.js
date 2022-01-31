00d749e974770cd72317a515437cb3e31ae546cf
dev0ps221 <seybaxscorpion@gmail.com>
2022-01-31 09:32:59 +0000

    events... triggers and callbacks... ngani ma ?

--- core/objects/events.js
+++ core/objects/events.js
@@ -2,23 +2,48 @@ const base = require('path').join(__dirname,'base')
 module.exports = class extends base{
 
 
-    registerCallBack(){
-
+    registerTrigger(tn,cb){
+        this.triggers[tn] = {
+            name:tn,
+            getCb:function(cb){
+                return cb
+            }
+        }
     }
 
-    forgetCallBack(){
-        
+    forgetTrigger(tn){
+        if(this.triggers.hasOwnProperty(tn)) delete(this.triggers[tn])
     }
 
-    removeAction(){
-
+    registerCallBack(tn,cb){
+        this.callbacks[tn] = cb
     }
 
-    addAction(){
+    forgetCallBack(tn){
+        if(this.callbacks.hasOwnProperty(tn)) delete(this.callbacks[tn])
+    }
 
+    removeAction(an){
+        if(this.actions.hasOwnProperty(an)) delete(this.actions[an])
+    }
+
+    addAction(an,af){
+        this.actions[an] = af
+    }
+
+    getCallBack(tn){
+        return this.callbacks.hasOwnProperty(tn)?this.callbacks[tn]:null
     }
 
     setBaseActions(){
+        const actions = [
+            [
+                'on',function(triggername,triggercb){
+                    this.registerCallBack(triggername,triggercb)
+                    this.registerTrigger(triggername,this.getCallBack(triggername))
+                }
+            ]
+        ]
         
     }
 
